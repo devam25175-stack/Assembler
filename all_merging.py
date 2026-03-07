758c4baa2672a4f4c17c0676c90fd4a9c917ad7b
Devam <devam25175@iiitd.ac.in>
2026-03-08 01:52:01 +0530

    Updated calling of the functions

--- all_merging.py
+++ all_merging.py
@@ -37,12 +37,12 @@ def final_halt(unit):
 
 # Finds the label in the instruction and seperates it from the rest of the instruction if found
 def label_finding(line):
-    if(":" not in line):
+    if(":" not in line):    # IF there is no label in the line
         return None, line.strip()
 
-    left, right = line.split(":", 1)
-    label = left.strip()
-    rest = right.strip()
+    left, right = line.split(":", 1)    # IF there is label in the line
+    label = left.strip()    #label
+    rest = right.strip()    #rest of the instruction
     return label, rest
 
 # Splits each instruction
@@ -50,7 +50,7 @@ def listing(instr):
     instr = instr.replace(",", " ")
     return instr.split()
 
-# Read the whole instruction set and makes a dictionary of labels with their memory addresses after each instruction
+# Read the whole instruction set and makes a dictionary of label as key and their memory addresses as values
 def label_table(lines):
     PC = 0
     labels = {}
@@ -59,11 +59,13 @@ def label_table(lines):
     for line in lines:
         label, rest = label_finding(line)
 
-        if(label != None):
+        if(label != None):      #if there is any label in the instruction
             if(not label or not label[0].isalpha()):    # Checking that label is not empty as well as starts with alphabet
                 raise ValueError(f"Invalid label {label} in line {idx}")
+            
             if(label in labels):        # No same label should be present
                 raise ValueError(f"Same label {label} in line {idx}")
+            
             labels[label] = PC
 
         if(rest != ""):     # Updating the PC if instuction is not empty
@@ -94,41 +96,50 @@ def assemble(lines, labels):
         # Identifing the correct instruction type and calling it
         if(mnemonic in R_TYPE):
             bin32 = r_type(unit, idx)
+
         elif mnemonic in I_TYPE:
             bin32 = i_type(unit, PC, labels, idx)
+
         elif mnemonic in S_TYPE:
             bin32 = s_type(unit, idx)
+
         elif mnemonic in B_TYPE:
-            bin32 = b_type(unit, PC, labels, idx)
+            if unit[3] in labels:
+                off = labels[unit[3]] - PC
+            else:
+                off = int(unit[3], 0)  # allows decimal immediate
+
+            bin32 = b_type(unit[0], unit[1], unit[2], off)
+            if bin32 is None:
+                raise ValueError(f"Invalid branch encoding line {idx}")
+            
         elif mnemonic in U_TYPE:
             bin32 = u_type(unit[0], unit[1], int(unit[2]))
+
         elif mnemonic in J_TYPE:
-            rd_name = unit[1]
-            target = unit[2]
 
-            if(target not in labels):
-                raise ValueError(f"Unknown label {target} at line {idx}")
+            if(unit[2] not in labels):
+                raise ValueError(f"Unknown label {unit[2]} at line {idx}")
 
-            offset = labels[target] - PC        # Finding the new memory address
-            bin32 = j_type(PC, rd_name, offset)
+            offset = labels[unit[2]] - PC        # Finding the new memory address
+            bin32 = j_type(unit[1], offset)
 
         if(len(bin32) != 32):
-            raise ValueError(f" Encoder must return 32-bit binary string at line {idx}")
+            raise ValueError(f"Encoder must return 32-bit binary string at line {idx}")
 
         output_lines.append(bin32)
         PC += 4
-
     return output_lines
 
 input_file = input("Enter input file: ")
 
-with open(input_file, "r") as f:
+with open(input_file, "r") as f:        #reading the file
     lines = f.readlines()
 
-lines = [ln.strip() for ln in lines if ln.strip() != ""]
-labels = label_table(lines)
+lines = [ln.strip() for ln in lines if ln.strip() != ""]        #removes spaces and newline characters and removes blank lines too
+labels = label_table(lines)     #dictionary with label as key and it's corresponding PC value as key
 
-instruction_lines = []
+instruction_lines = []      #this will contain only the instructions, without any label
 
 # Keeps only the actual instructions
 for line in lines:
@@ -136,7 +147,7 @@ for line in lines:
     if(rest != ""):
         instruction_lines.append(rest)
 
-# Checking if there are instructions or not
+# Checking if there are only label and not any instructions Eg:- loop:
 if(not instruction_lines):
     print("No instructions found")
     exit()
@@ -147,7 +158,7 @@ if(not final_halt(listing(instruction_lines[-1]))):
     exit()
 
 try:
-    out_lines = assemble(lines, labels)
+    out_lines = assemble(instruction_lines, labels)
     for line in out_lines:
         print(line)
 except Exception as e:
