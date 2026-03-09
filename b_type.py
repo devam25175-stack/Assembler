ad6e531931aa35759d56339e2f6905bff700c6b4
devjindal392-max <devjindal392@gmail.com>
2026-03-09 21:39:39 +0530

    b-type instruction

--- b_type.py
+++ b_type.py
@@ -1,28 +1,28 @@
-def b_type(inst,r1,r2,off):
+branch_map={"beq":"000","bne":"001","blt":"100","bge":"101","bltu":"110","bgeu":"111"}
 
-    opcode="1100011"
+def b_type(inst, r1, r2, off):
 
-    # convertd the  register names to 5-bits binary using the  reg_map
-    rs1=format(reg_map[r1],"05b")
-    rs2=format(reg_map[r2],"05b")
+    opcode = "1100011"
 
-    funct3=branch_map[inst]
+    rs1 = format(reg_map[r1], "05b")
+    rs2 = format(reg_map[r2], "05b")
 
-    # branch offsets must be multiples of 2
+    funct3 = branch_map[inst]
+
     if off % 2 != 0:
         raise ValueError("Branch offset must be even")
 
-    # correct byte range
     if off < -4096 or off > 4094:
         raise ValueError("branch immediate out of range")
-    
-    off = off >> 1
 
-    if off<0:
-        off=(1<<12)+off
+    if off < 0:
+        off = (1 << 13) + off
 
-    imm=format(off,"012b")  # converting the  offset to 12-bits binary
+    imm = format(off, "013b")
 
-    out = imm[0] + imm[2:8] + rs2 + rs1 + funct3 + imm[8:12] + imm[1] + opcode
+    imm12 = imm[0]
+    imm10_5 = imm[2:8]
+    imm4_1 = imm[8:12]
+    imm11 = imm[1]
 
-    return out
+    return imm12 + imm10_5 + rs2 + rs1 + funct3 + imm4_1 + imm11 + opcode
